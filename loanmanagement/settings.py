8b722829336dc502d2e02ee7234f49867071cc2c
kevin99-oop <kevinpandya18@gnu.ac.in>
2022-04-04 12:31:03 +0530

    security_update_v.1

--- loanmanagement/settings.py
+++ loanmanagement/settings.py
@@ -203,12 +203,4 @@ SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
 SECURE_SSL_REDIRECT = True
 
-# Content Security Policy
-CSP_DEFAULT_SRC = ("'none'", )
-CSP_STYLE_SRC = ("'self'", )
-CSP_SCRIPT_SRC = ("'self'", )
-CSP_IMG_SRC = ("'self'", )
-CSP_FONT_SRC = ("'self'", )
-
-
 
