5dc8a39091ec92b9ab696292227fc4e98865a2f2
kevin99-oop <kevinpandya18@gnu.ac.in>
2022-04-06 18:38:13 +0530

    signup_change_document

--- customer/forms.py
+++ customer/forms.py
@@ -14,4 +14,4 @@ class CustomerUserForm(forms.ModelForm):
 class CustomerForm(forms.ModelForm):
     class Meta:
         model=models.Customer
-        fields=['address','mobile','profile_pic']
+        fields=['address','mobile','profile_pic','credit_score']
--- customer/models.py
+++ customer/models.py
@@ -10,8 +10,8 @@ class Customer(models.Model):
     address = models.CharField(max_length=40)
     mobile = models.CharField(max_length=20,null=False)
     card_text = models.CharField(max_length=150)
+    credit_score = models.CharField(max_length=3,null=False)
     
-   
     @property
     def get_name(self):
         return self.user.first_name+" "+self.user.last_name
