33493fae946373efbfdd77d6beb01e685a16114f
kevin99-oop <kevinpandya18@gnu.ac.in>
2022-04-25 17:29:54 +0530

    pwd_reset_sovled_2

--- customer/test/test_urls.py
+++
@@ -1,9 +0,0 @@
-from django.test import SimpleTestCase
-from django.urls import reverse, resolve
-from customer.views import customer_signup_view
-
-class TestUrls(SimpleTestCase):
-    def test_signup(self):
-        url = reverse('customersignup')
-        print(resolve(url))
-        self.assertEquals(resolve(url).func, customer_signup_view)
